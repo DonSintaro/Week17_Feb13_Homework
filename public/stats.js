2bbd9f2238721a83902d08907bcb80f4f76733e7
DonSintaro <jarriaga.important@gmail.com>
2020-02-22 16:09:35 -0600

    changed front-end code a little

--- public/stats.js
+++ public/stats.js
@@ -190,10 +190,8 @@ function duration(data) {
   let durations = [];
 
   data.forEach(workout => {
-    workout.exercises.forEach(exercise => {
-      durations.push(exercise.duration);
+    durations.push(workout.totalDuration);
     });
-  });
 
   return durations;
 }
